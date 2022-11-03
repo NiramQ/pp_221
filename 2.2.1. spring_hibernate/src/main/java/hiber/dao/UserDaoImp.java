7b3b4cbcc38f240f5d332ccb54cd493aee47e1a2
Andrew <niramrus@gmail.com>
2022-11-03 21:32:45 +0500

    fixed codestyle

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class UserDaoImp implements UserDao {
 
    private SessionFactory sessionFactory;
+
    @Autowired
    public void setSessionFactory(SessionFactory sessionFactory) {
       this.sessionFactory = sessionFactory;
