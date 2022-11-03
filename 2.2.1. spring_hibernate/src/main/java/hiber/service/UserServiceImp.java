4da1a349e5d08b8fff66a89f2cd592ce67f62f93
Andrew <niramrus@gmail.com>
2022-11-03 20:53:08 +0500

    fix @Transactional

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+
 public class UserServiceImp implements UserService {
 
    private UserDao userDao;
@@ -18,19 +20,16 @@ public class UserServiceImp implements UserService {
       this.userDao = userDao;
    }
 
-   @Transactional
    @Override
    public void add(User user) {
       userDao.add(user);
    }
 
-   @Transactional
    @Override
    public User getUserByCar(String model, int series) {
       return userDao.getUserByCar(model, series);
    }
 
-   @Transactional(readOnly = true)
    @Override
    public List<User> getListUsers() {
       return userDao.getListUsers();
